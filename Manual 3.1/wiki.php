d14608a5082bfdcab12700486a10bf39a211574f
iduda <Sampaiocdc1@gmail.com>
2017-04-13 00:54:59 -0300

    atualização wiki

---
+++ Manual 3.1/wiki.php
@@ -0,0 +1,73 @@
+
+
+<html>
+   <head>
+      <title>IFPE-IFPE Instituto Federal de Pernambuco</title>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="style1.css">
+   </head>
+   <body>
+      <header class="topo">
+         <div class= "menu">
+            <ul>
+               <li>IFPE</li>
+               <h1 class ="portal" ><a  href="http://www.ifpe.edu.br" class ="link1">Instituto Federal  de Pernambuco</a></h1>
+               <li>Campus Igarassu</li>
+            </ul>
+             
+         </div>
+        
+         
+         
+        </header>
+        
+         <?php
+include("menu.php");
+?>
+        
+      <section>
+       
+  <h1 >Instruções MediaWiki</h1>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  
+
+<table>
+<tr>
+   <td>Para usar a mediawiki você tem que baixar o arquivo que pode ser acessado:
+          <a href="https://drive.google.com/drive/folders/0B-3_TsDjImY0Tm9UQjhqSVpkU1U?usp=sharing" class="link2">Aqui</a></td>
+    </tr>
+
+  <tr>
+   <td>Após ele ser baixado descompacte e salve no diretorio raiz do seu servidor o htdocs;
+   crie uma base de dados e baixe o arquivo wiki.sql que pode ser acessado:
+   <a href="https://drive.google.com/open?id=0B-3_TsDjImY0TV9BNmpDQXltb28" class="link2">Aqui</a></td>
+  </tr>
+  <tr>
+   <td> Agora importe o arquivo sql e salve em seu servidor, onde nele está inserido o usuario e a senha de mediawiki ;
+vá no browser e digite o caminho em que sua pasta foi salva  e vai aparecer a nossa wiki.
+      
+   </td>
+  </tr>
+
+
+ </table>
+
+</section>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+  <?php
+    include("rodape.php");
+     ?>
+   </body>
+</html>
+
