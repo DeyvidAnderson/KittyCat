9171e818e1f057c3fa45a902cb55b025518a23bb
Amandaalmeida12 <amandaisabel237@gmail.com>
2017-04-24 21:53:47 -0300

    atualização do codigo

--- Manual 3.1/menu.php
+++ Manual 3.1/menu.php
@@ -1,14 +1,12 @@
 
-
-
 <?php
-$menu_Inicio = "<a href='../index.php/.'>Inicio</a>";
-$menu_Tutoriais = "<a href='tutoriais.php../'>Tutoriais</a>";
+$menu_Inicio = "<a href='index.php'>Inicio</a>";
+
 $menu_Tutoriais = "<a href='tutoriais.php'>Tutoriais</a>";
 
 
 
-$menu_wiki="<a href='../wiki.php/.'>Mediawiki</a>";
+$menu_wiki="<a href='wiki.php'>Mediawiki</a>";
 
 $mostra_menus = "<div id='opcoes'><ul>
   
